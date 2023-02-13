a4155e4d5d37cd45893d63acb74abddf1204274e
Wilhelm Ruiz <wilhelm@gmail.com>
2023-02-13 16:36:45 -0500

    Configuraciones de proyecto

---
+++ WebCliente/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebCliente.Models;
+
+namespace WebCliente.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebCliente/Controllers/PrincipalController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCliente.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        private readonly ILogger<PrincipalController> _logger;
+
+        public PrincipalController(ILogger<PrincipalController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Principal()
+        {
+            return View();
+        }
+    }
+}
